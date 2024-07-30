6a55da8d0f602022bdf93e8055597a776a1ad0b3
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2024-07-30 19:15:40 +0200

    Fixed cloud token not reading from env. Added Replit stuff to gitignore

--- headers/defines.h
+++ headers/defines.h
@@ -2,10 +2,12 @@
 #ifndef H_MLN_DEFINES_H
 #define H_MLN_DEFINES_H
 
+#include <cstdlib>
+
 #ifdef MLN_DB_DISCORD_BOT_TOKEN
     #define DISCORD_BOT_TOKEN MLN_DB_DISCORD_BOT_TOKEN
 #else
-    #define DISCORD_BOT_TOKEN CLD_DISCORD_BOT_TOKEN
+    #define DISCORD_BOT_TOKEN std::getenv("CLD_DISCORD_BOT_TOKEN")
 #endif
 
 #endif
