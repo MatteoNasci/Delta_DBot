8408b3ba68df65dafc56fdfabdd1f879cd49212d
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2024-08-06 19:22:43 +0200

    Added db cmds prototypes (to be implemented)

--- source/commands/db.cpp
+++ source/commands/db.cpp
@@ -2,18 +2,32 @@
 #include "sqlite3.h"
 
 #include <variant>
-#include <unordered_set>
+#include <unordered_map>
+
+enum class db_op : char {
+    db_none = 0,
+    db_insert = 1,
+    db_select = 2,
+    db_show_records_keys = 3,
+    db_update = 4,
+    db_remove = 5,
+    db_set_db_channel = 6,
+    db_force_db_save = 7
+};
 
 dpp::task<void> mln::db::command(mln::bot_delta_data_t& data, const dpp::slashcommand_t& event){
     const bool broadcast = std::get<bool>(event.get_parameter("broadcast"));
     const dpp::command_interaction cmd_data = event.command.get_command_interaction();
     
-    static const std::unordered_set<std::string> allowed_sub_commands{
-        {"insert"},
-        {"select"},
-        {"show records"},
-        {"update"},
-        {"remove"}
+    static const std::unordered_map<std::string, db_op> allowed_sub_commands{
+        {"insert", db_op::db_insert},
+        {"select", db_op::db_select},
+        {"show_records_keys", db_op::db_show_records_keys},
+        {"update", db_op::db_update},
+        {"remove", db_op::db_remove},
+        {"set_db_channel", db_op::db_set_db_channel},
+        {"force_db_save", db_op::db_force_db_save},
+        {"", db_op::db_none},
     };
 
     auto waiting_res = event.co_thinking(!broadcast);
@@ -36,22 +50,34 @@ dpp::task<void> mln::db::command(mln::bot_delta_data_t& data, const dpp::slashco
         msg.set_flags(dpp::m_ephemeral);
     }
     event.reply(msg);*/
+    co_await waiting_res;
+    event.edit_response("Work in progress!");
     co_return;
 }
 dpp::slashcommand mln::db::get_command(dpp::cluster& bot){
     return dpp::slashcommand(mln::db::get_command_name(), "Manage the database.", bot.me.id)
-        .add_option(dpp::command_option(dpp::co_boolean, "broadcast", "Broadcast result to the channel.", true))
-        .add_option(dpp::command_option(dpp::co_sub_command, "insert", "Inserts a new record in the db. It will fail if the given name is not unique!", false)
-            .add_option(dpp::command_option(dpp::co_attachment, "file", "File to insert.", true))
-            .add_option(dpp::command_option(dpp::co_attachment, "name", "Unique name to associate with the file.", true)))
-        .add_option(dpp::command_option(dpp::co_sub_command, "select", "Selects a record from the db. It will fail if the given name is not present!", false)
-            .add_option(dpp::command_option(dpp::co_attachment, "name", "Unique name to associate with the file.", true)))
-        .add_option(dpp::command_option(dpp::co_sub_command, "show records", "Shows all the db records names.", false))
-        .add_option(dpp::command_option(dpp::co_sub_command, "update", "Updates an existing record in the db. It will fail if the given name is not present!", false)
-            .add_option(dpp::command_option(dpp::co_attachment, "file", "File to insert.", true))
-            .add_option(dpp::command_option(dpp::co_attachment, "name", "Unique name to associate with the file.", true)))
-        .add_option(dpp::command_option(dpp::co_sub_command, "remove", "Removes an existing record in the db. It will fail if the given name is not present!", false)
-            .add_option(dpp::command_option(dpp::co_attachment, "name", "Unique name to associate with the file.", true)));
+        .add_option(dpp::command_option(dpp::co_sub_command_group, "op", "Perform an operation on the db", false)
+            .add_option(dpp::command_option(dpp::co_sub_command, "insert", "Inserts a new record in the db. It will fail if the given name is not unique!", false)
+                .add_option(dpp::command_option(dpp::co_attachment, "file", "File to insert.", true))
+                .add_option(dpp::command_option(dpp::co_attachment, "name", "Unique name to associate with the file.", true))
+                .add_option(dpp::command_option(dpp::co_boolean, "broadcast", "Broadcast result to the channel. Default: false", false)))
+            .add_option(dpp::command_option(dpp::co_sub_command, "select", "Selects a record from the db. It will fail if the given name is not present!", false)
+                .add_option(dpp::command_option(dpp::co_attachment, "name", "Unique name to associate with the file.", true))
+                .add_option(dpp::command_option(dpp::co_boolean, "broadcast", "Broadcast result to the channel. Default: false", false)))
+            .add_option(dpp::command_option(dpp::co_sub_command, "show_records_keys", "Shows all the db records names.", false)
+                .add_option(dpp::command_option(dpp::co_boolean, "broadcast", "Broadcast result to the channel. Default: false", false)))
+            .add_option(dpp::command_option(dpp::co_sub_command, "force_db_save", "Forces the db to save in the designated channel, instead of waiting for a timer.", false)
+                .add_option(dpp::command_option(dpp::co_boolean, "broadcast", "Broadcast result to the channel. Default: false", false)))
+            .add_option(dpp::command_option(dpp::co_sub_command, "update", "Updates an existing record in the db. It will fail if the given name is not present!", false)
+                .add_option(dpp::command_option(dpp::co_attachment, "file", "File to insert.", true))
+                .add_option(dpp::command_option(dpp::co_attachment, "name", "Unique name to associate with the file.", true))
+                .add_option(dpp::command_option(dpp::co_boolean, "broadcast", "Broadcast result to the channel. Default: false", false)))
+            .add_option(dpp::command_option(dpp::co_sub_command, "remove", "Removes an existing record in the db. It will fail if the given name is not present!", false)
+                .add_option(dpp::command_option(dpp::co_attachment, "name", "Unique name to associate with the file.", true))
+                .add_option(dpp::command_option(dpp::co_boolean, "broadcast", "Broadcast result to the channel. Default: false", false)))
+            .add_option(dpp::command_option(dpp::co_sub_command, "set_db_channel", "Sets the channel where the bot will store the db. Required before any db cmd!", false)
+                .add_option(dpp::command_option(dpp::co_channel, "channel", "Channel the bot will use to store the database.", true))
+                .add_option(dpp::command_option(dpp::co_boolean, "broadcast", "Broadcast result to the channel. Default: false", false))));
 }
 std::string mln::db::get_command_name(){
     return "db";
