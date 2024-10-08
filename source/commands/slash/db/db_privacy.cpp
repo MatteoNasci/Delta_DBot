ef9ffa0df3352a453b0225378ee4ebc050d6ff73
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2024-10-08 20:56:21 +0200

    Added missing co_await, updated privacy policy to include mog db stuff

--- source/commands/slash/db/db_privacy.cpp
+++ source/commands/slash/db/db_privacy.cpp
@@ -29,9 +29,10 @@ Our bot collects the following types of information:
 
 - **User ID**: Your unique Discord user identifier.
 - **Guild ID**: The unique identifier of the server (guild) where the bot is used.
-- **Channel ID**: The unique identifier of the channel used by the bot to store data.
+- **Channel ID**: The unique identifier of the channel used by the bot to store data or associated to a MoG team.
+- **Role ID**: The unique identifier of the role associated to a MoG team.
 - **Message Content URL**: A link to a copy of the content of messages you send to Delta's database.
-- **Name**: The name of the database records you create with the bot.
+- **Name**: The name of the database records you create with the bot or associated to a MoG team.
 - **Description**: The description you provide for database records.
 - **Issue Text**: The text you provide when issuing a `/report_issue` command.
 
@@ -48,19 +49,23 @@ You have the right to request the deletion of your information. If you wish to r
   1. Ensure no one in your guild can use the bot's commands.
   2. Use the command `/db delete guild` in a channel of the guild you wish to remove from the database.
   3. Use the command `/db config update_dump_channel` without specifying the channel parameter.
-  4. Remove the bot from your guild.
+  4. Remove all existing MoG teams using the command `/mog team delete` (to see all existing teams use `/mog team show`)
+  5. Remove the bot from your guild.
 
 - **For Users: To remove all your records from a specific Discord server:**
   1. Use the command `/db delete user` in a channel of the guild from which you wish to remove your records. Ensure you specify yourself as the user parameter.
+  2. Use the command `/mog team leave` for all the teams you are in.
 
 - **For Users: To remove all your records from all servers:**
   1. Use the command `/db delete self` in any channel of any guild where the bot has access.
+  2. Use the command `/mog team leave` for all the teams in any guild you are in.
 
 Please note that even after using these commands, some data may be retained for a period of time, such as:
 
 - **Guild ID**: The unique identifier of the server (guild) where the bot is used. After following the steps for the admin to remove the records related to the server, this information might still remain saved in the database temporarily. A future update will include a command to remove this information as well.
 - **Issue Text**: Text sent to the bot via the `/report_issue` command will be stored for some time.
 - **Message Content**: When creating a record, the bot saves a copy of the Discord message content in a dedicated channel and stores a link to that message in the database. The bot will attempt to delete these stored messages when the associated record is deleted. However, this may not always succeed due to the bot no longer having permission to modify the stored message or channel, or for other reasons beyond the bot's control. Users will be notified if one or more messages could not be deleted.
+)""").add_field("", R"""(
 
 **5. Data Security**
 
@@ -78,7 +83,7 @@ If you have any questions or concerns about this Privacy Policy or our data prac
 
 By using our bot, you consent to the collection, use, and storage of your data as described in this Privacy Policy.)"""));
 
-    mln::response::co_respond(cmd_data.data, s_info, false, "Failed to reply with the db privacy text!");
+    co_await mln::response::co_respond(cmd_data.data, s_info, false, "Failed to reply with the db privacy text!");
     co_return;
 }
 
